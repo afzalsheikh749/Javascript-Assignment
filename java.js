dc1e554a96508e5741d41933c769cc4169f52be3
Afzal Hussain <mdafzal297922@gmail.com>
2025-08-21 03:33:45 +0500

    Javascript Object

---
+++ java.js
@@ -0,0 +1,149 @@
+console.log("welcome to javascript");
+
+// Chapter No 1 (Alert) ************
+
+// Q No 1
+
+// alert("Welcome to Javasript code");
+
+// Q No 2
+
+// alert("Error ! please inter a valid password!");
+
+// Q No 3
+
+// alert("Welcome to JS land... \n Happy Coding!");
+
+// Q No 4
+
+//  alert("Welcome to JS land..");
+// alert("Happy Coding! \n Prevent this page from creating additional dialogs");
+
+// Q No 5
+
+// alert("Hello... I can run Js through my web browser's console");
+
+// Q No 7
+
+// document.write("<h1> Hello javascript user <h1>");
+
+// Chapter No 2  (Varriable for strings) ***************
+
+// Q No 1
+
+var name = "Ahemad " ;
+console.log(name);
+
+// Q No 2
+
+var firstName = "Afzal" ;
+var lastName = "Hussain" ;
+
+console.log( firstName +  " "  +lastName);
+
+// Q No 3
+  
+var name = "Hello world";
+
+// alert(name);
+
+// Q  No 4
+
+var stdName = "Jhone doe" ;
+var yearOld = 15 ;
+var aboutIn = "Certified mobile application development";
+
+// alert(stdName);
+// alert(yearOld + " yearold");
+// alert(aboutIn);
+ 
+// Q No 5
+
+var list = " Pizza \n Pizz \n Piz \n Pi \n P" ;
+// alert(list) ;
+
+// Q No 6 
+
+var email = "mdafzal297922@gmail.com"
+//  alert(email + "");
+
+// Q No 7
+
+// alert(" I am trying to learn from the Book A Smarter \n way to learn Javasript");
+
+// Q No 8
+
+// document.write (" <h1>Yah ! I can write HTML content through Javascript</h1>");
+
+var show = "" ;
+// alert(show);
+
+// chapter No 3  Varriable for Number  ***********
+
+// Q No 1
+
+var name = "I am ";
+var age  = 15 ;
+var year = " year old";
+
+// alert(name + age + year);
+
+// Q No 2
+
+var visit = "You have visited this site ";
+var number = 14;
+var time   = " times";
+
+// alert(visit + number + time);
+
+// Q No 3
+
+//  document.write("<p> My birth year is 1990 <br> Data type of my declared varriable is number </p>") ;
+
+// Q No 4
+
+var visitorName = "Jhone doe" ;
+var productTitle = "T-shirt";
+var quantity  = 5 ;
+
+// document.write(visitorName + " ordered" + quantity + productTitle + " (S) on XYZ clothing store" );
+
+
+// Chapter No # 4   LEGAL *&* ILLEGAL **********
+
+// Q No 1
+
+var name ="Abdul ", age = 25  , fullName = " Abdulbinkamran" ;
+ console.log(name+age+fullName); 
+
+//  Q No 2
+
+// legal
+
+// var firstName = "afzal" ;
+// var _userName  = "hussain";
+// var $rate      =   "2"   ;
+// var roll_number =    "1"   ;
+// var email123    = "email";
+
+ 
+// Illegal
+
+// var 1stItem = "";
+// user name =   "";
+// var if    =   "";
+// var my-variable = "" ;
+// var @email ="" ;
+
+// Q No 3
+
+document.write( "<h1> Rules For naming JS varriables </h1> <br> <br><br>" );
+
+document.write("Varriable names can only content , numbers , $ and _ .For Examples : @Smy_1stVarriable <br>");
+document.write("Varrible must begin with a letter $ or _ . For example : $name, _name or name <br>")
+document.write("Varriable names are case sensitive <br>" );
+document.write ("Varriable name should not be  JS  keywords <br> ");
+
+
+
+// ************ THE  END ********************
