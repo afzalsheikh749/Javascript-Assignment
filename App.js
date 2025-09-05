454b3e9941619f25049de0f6c331d05e22dc97f0
Afzal Hussain <mdafzal297922@gmail.com>
2025-09-05 15:41:55 +0500

    Add files via upload

---
+++ App.js
@@ -0,0 +1,75 @@
+console.log("Welcome");
+
+// Assignment No # 8
+// Q No # 01
+
+// var inter = prompt("Enter a Number or letter :");
+
+// var asiiCode = inter.charCodeAt(0);
+
+// if( asiiCode >= 48 && asiiCode <= 57){
+//    alert("Type Your Enter Number");
+
+// }   else if(asiiCode >= 65  && asiiCode <= 90){
+//       alert("Type Your Enter uppercase letter");
+
+//  }  else if(asiiCode >= 97  && asiiCode <= 122) {
+//       alert("Type Your Enter lowercase letter");
+
+//   } else {
+//     alert("Not a number or Letter");
+// }
+
+// Q No # 02
+
+// var num1  = prompt("Type your first number");
+// var num2  = prompt("Type your second number");
+
+// if( num1 > num2){
+//     console.log(num1 + " is larger than " + num2);
+
+// } else if (num2 > num1){
+//      console.log(num2 + " is larger than " + num1);
+
+// }else{
+//     console.log("Both number are equal");
+// }
+
+// Q No # 03
+
+// var num = prompt("Type your Number")
+
+// if(num > 0){
+//     console.log("Number is Positive " + num);
+
+// } else if(num < 0){
+//     console.log("Number is Negative " + num);
+// } else{
+//     console.log("Number is Zero");
+// }
+
+// Q No # 04
+
+// var check = prompt("Type vowel character");
+// var check = check.charAt();
+
+// if( check === "a" || check === "i" || check === "o" ||check === "u" ||check === "e" ) {
+//     console.log("This is vowel word");
+
+// } else{
+//     console.log("This is not vowel word");
+// }
+
+// Q No # 05
+
+// var password = "hussain123" ;
+// var userpassword = prompt("Type  your password");{
+
+//  if(password === userpassword){
+//     alert("correct password welcome to inside !");
+
+// } else{
+//     alert("Incorrect password");
+
+// }
+// }
